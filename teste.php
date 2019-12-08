102bab42f0e35e0e10f0ffe121cb90ee4c1538a9
Otavio Mota <otaviomota@gmail.com>
2019-12-07 22:21:00 -0200

    adicionando o arquivo teste.php

---
+++ teste.php
@@ -0,0 +1,10 @@
+<?php
+
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loader = new Twig\Loader\FilesystemLoader('templates/');
+
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('view.twig');
